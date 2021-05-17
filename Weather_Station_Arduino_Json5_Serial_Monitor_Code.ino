2ed806186d40e854bc85d0a63c76d999fd24591c
Sreeram Aj <sreeramzeno@gmail.com>
2021-05-18 01:40:07 +0530

    Add files via upload

---
+++ Weather_Station_Arduino_Json5_Serial_Monitor_Code.ino
@@ -0,0 +1,127 @@
+/*feel free to contact
+ * sreeramaj53@gmail.com
+ * www.youtube.com/ZenoModiff
+ * last updated - time 01:37am - date 18 may 2021
+ * Github Link :-- https://github.com/Zenomodiff/Weather-Station-With-Arduino-Nodemcu
+ */
+ 
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#include "DHT.h"
+
+#define DHTPIN 12
+#define DHTTYPE DHT22
+Adafruit_BMP280 bmp; 
+
+SoftwareSerial s_serial_to_esp(3, 2);
+
+const int mq2 = A0; 
+const int mq4 = A1; 
+const int mq5 = A2; 
+const int mq135 = A3; 
+const int rain = A6; 
+float Pressure;
+float Altitude;
+int ldr = 11;
+
+float m = -0.6527;  
+float b = 1.30;  
+float R0 = 21.91; 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  
+  pinMode(mq2, INPUT);
+  pinMode(mq4, INPUT); 
+  pinMode(mq5, INPUT);
+  pinMode(mq135, INPUT);
+  pinMode(rain, INPUT);
+  pinMode(ldr, OUTPUT); 
+
+  Serial.begin(9600);
+  
+  while (!Serial) continue;
+  
+  s_serial_to_esp.begin(4800);
+
+   if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    while (1) delay(10);
+    Serial.flush();
+  }
+
+   dht.begin();
+   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,   
+               Adafruit_BMP280::SAMPLING_X2,    
+               Adafruit_BMP280::SAMPLING_X16,   
+               Adafruit_BMP280::FILTER_X16, 
+               Adafruit_BMP280::STANDBY_MS_500);
+}
+
+void loop() {
+  
+  double mq2_value = analogToPPM(analogRead(mq2));
+  double mq4_value = analogToPPM(analogRead(mq4));
+  double mq5_value = analogToPPM(analogRead(mq5));
+  double mq135_value = analogToPPM(analogRead(mq135));
+  double rain_value = analogToPPM(analogRead(rain));
+  int ldrvalue = analogRead(ldr);
+  int h = dht.readHumidity();
+  int t = dht.readTemperature();
+  int f = dht.readTemperature(true);
+  int pressure = (bmp.readPressure());
+  Pressure = pressure;
+  int altitude =  (bmp.readAltitude(1013.25)); 
+  Altitude = altitude; 
+
+DynamicJsonBuffer jBuffer;
+JsonObject& root = jBuffer.createObject();
+
+  root["mq2_value"] = mq2_value;
+  root["mq4_value"] = mq4_value;
+  root["mq5_value"] = mq5_value;
+  root["mq135_value"] = mq135_value;
+  root["rain_value"] = rain_value;
+  root["t"] = t;
+  root["h"] = h;
+  root["ldrvalue"] = ldrvalue;
+  root["Pressure"] = Pressure;
+  root["Altitude"] = Altitude;
+  root.prettyPrintTo(s_serial_to_esp);
+  Serial.println();
+
+  Serial.println("************************************");
+  Serial.print("MQ-2 Smoke: ");Serial.print(mq2_value, DEC);Serial.println(" PPM");
+  Serial.print("MQ-4 Cng: ");Serial.print(mq4_value, DEC);Serial.println(" PPM");
+  Serial.print("MQ-5 Lpg: ");Serial.print(mq5_value, DEC);Serial.println(" PPM");
+  Serial.print("MQ-135 Aiq: ");Serial.print(mq135_value, DEC);Serial.println(" PPM");
+  Serial.print("RAIN Value: ");Serial.print(rain_value, DEC);Serial.println(" MM");
+  Serial.print("DHT:-Temperature ");Serial.print(t);Serial.println(" C");
+  Serial.print("DHT:-Humidity ");Serial.print(h);Serial.println(" %");
+  Serial.print("LDR: ");Serial.print(ldrvalue, DEC); Serial.println(" LX");
+  Serial.print("BMP280:-Pressure ");Serial.print(Pressure); Serial.println(" PA");
+  Serial.print("BMP280:-Altitude ");Serial.print(Altitude);Serial.println(" M");
+  Serial.println("************************************\n\n");
+
+  delay(3000);
+}
+
+  double analogToPPM(int aValue) {
+  float sensor_volt;  
+  float RS_gas;   
+  float ratio; 
+  int sensorValue = aValue; 
+
+  sensor_volt = sensorValue*(5.0/1023.0); 
+  RS_gas = ((5.0*10.0)/sensor_volt)-10.0; 
+  ratio = RS_gas/R0;  
+
+  double ppm_log = (log10(ratio)-b)/m; 
+
+  return ppm_log;
+}
---
+++ Weather_Station_Nodemcu_Json5_Serial_Monitor_Code.ino
@@ -0,0 +1,92 @@
+#include <ESP8266WiFi.h>
+#include "ArduinoJson.h"
+#include <SoftwareSerial.h> 
+
+SoftwareSerial s_serial_to_nano(2,0); 
+
+const char *ssid =  "Zeno";     
+const char *pass =  "9061116368";
+
+WiFiClient client;
+
+float mq2_value_global;
+float mq4_value_global;
+float mq5_value_globak;
+float mq135_value_global;
+float rain_value_global;
+float t_global;
+float h_global;
+float ldr_value_global;
+float Pressure_global;
+float Altitude_global;
+
+int Wifi_Led = 14;
+
+void setup() {
+
+  Serial.begin(9600);
+      
+  Serial.println("-------------------------");
+  Serial.println(" Weather Station  ");
+  Serial.println("-------------------------");
+
+  pinMode(Wifi_Led, OUTPUT);
+  digitalWrite(Wifi_Led, LOW);
+
+ Serial.println("Connecting to ");
+ Serial.println(ssid); 
+ 
+ WiFi.begin(ssid, pass); 
+ while (WiFi.status() != WL_CONNECTED) 
+ { 
+  delay(500);
+  Serial.print(".");
+ }
+ Serial.println("");
+ Serial.println("WiFi connected"); 
+ digitalWrite(Wifi_Led, HIGH);
+ s_serial_to_nano.begin(4800);
+
+}
+
+void loop() {
+  
+  if (s_serial_to_nano.available()) {
+ DynamicJsonBuffer jsonBuffer(1024); 
+ JsonObject& root = jsonBuffer.parseObject(s_serial_to_nano);
+if(!root.success()){
+  return;
+}
+JsonArray& data = root["data"];
+
+
+float mq2_value = root["mq2_value"];
+float mq4_value = root["mq4_value"];
+float mq5_value = root["mq5_value"];
+float mq135_value = root["mq135_value"];
+float rain_value = root["rain_value"];
+float t = root["t"];
+float h = root["h"];
+int ldrvalue = root["ldrvalue"];
+int Pressure = root["Pressure"];
+int Altitude = root["Altitude"];
+
+ 
+ Serial.println();
+ Serial.print("1. Smoke ");Serial.print(mq2_value);Serial.println(" PPM");
+ Serial.print("2. cng ");Serial.print(mq4_value);Serial.println(" PPM");
+ Serial.print("3. lpg ");Serial.print(mq5_value);Serial.println("PPM");
+ Serial.print("4. airquality ");Serial.print(mq135_value);Serial.println(" PPM");
+ Serial.print("5. rainvalue ");Serial.print(rain_value);Serial.println(" MM");
+ Serial.print("6. temperature ");Serial.print(t);Serial.println(" C");
+ Serial.print("7. humidity ");Serial.print(h);Serial.println(" %");
+ Serial.print("8. ldr ");Serial.print(ldrvalue);Serial.println(" LX");
+ Serial.print("9. Pressure ");Serial.print(Pressure);Serial.println(" PA");
+ Serial.print("10. Altitude ");Serial.print(Altitude);Serial.println(" M");
+ Serial.println();
+
+ while (s_serial_to_nano.available() > 0)
+ s_serial_to_nano.read();
+
+}
+}
