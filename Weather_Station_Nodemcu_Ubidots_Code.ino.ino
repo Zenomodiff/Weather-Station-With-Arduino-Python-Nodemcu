bab0545cd2df893f0ae4bda6c71ff85de41074aa
Sreeram Aj <sreeramzeno@gmail.com>
2021-05-24 17:07:09 +0530

    Add files via upload

---
+++ Weather_Station_Nodemcu_Ubidots_Code.ino.ino
@@ -0,0 +1,115 @@
+/*feel free to contact
+ * sreeramaj53@gmail.com
+ * www.youtube.com/ZenoModiff
+ * last updated - time 05:04 PM - date 24 may 2021
+ * Github Link :-- https://github.com/Zenomodiff/Weather-Station-With-Arduino-Nodemcu
+ */
+
+#include <ESP8266WiFi.h>
+#include "ArduinoJson.h"
+#include "Ubidots.h"
+#include <SoftwareSerial.h> 
+
+SoftwareSerial s_serial_to_nano(2,0); 
+
+const char* UBIDOTS_TOKEN = "PASTE UBIDOTS TOKEN"; 
+const char *ssid =  "TYPE SSID";     
+const char *pass =  "TYPE PASSWORD";
+
+Ubidots ubidots(UBIDOTS_TOKEN, UBI_HTTP);
+
+
+float mq2_value_global;
+float mq4_value_global;
+float mq5_value_globak;
+float mq135_value_global;
+float rain_value_global;
+float t_global;
+float h_global;
+float ldr_value_global;
+float Pressure_global;
+float Altitude_global;
+
+int Wifi_Led = 14;
+
+void setup() {
+
+Serial.begin(9600);
+      
+Serial.println("-------------------------");
+Serial.println(" Weather Station  ");
+Serial.println("-------------------------");
+delay(2000);
+
+pinMode(Wifi_Led, OUTPUT);
+digitalWrite(Wifi_Led, LOW);
+ 
+ WiFi.begin(ssid, pass); 
+ ubidots.wifiConnect(ssid, pass);
+ while (WiFi.status() != WL_CONNECTED) 
+ { 
+  delay(500);
+  Serial.print(".");
+ }
+ Serial.println(""); 
+ digitalWrite(Wifi_Led, HIGH);
+ s_serial_to_nano.begin(4800);
+
+}
+
+void loop() {
+  
+  if (s_serial_to_nano.available()) {
+  DynamicJsonDocument doc(1024);
+  DeserializationError err = deserializeJson(doc, s_serial_to_nano);
+  if (err == DeserializationError::Ok) {
+
+float mq2_value = doc["mq2_value"];
+float mq4_value = doc["mq4_value"];
+float mq5_value = doc["mq5_value"];
+float mq135_value = doc["mq135_value"];
+float rain_value = doc["rain_value"];
+float t = doc["t"];
+float h = doc["h"];
+int ldrvalue = doc["ldrvalue"];
+int Pressure = doc["Pressure"];
+int Altitude = doc["Altitude"];
+
+  ubidots.add("Smoke", mq2_value); 
+  ubidots.add("Cng", mq4_value);
+  ubidots.add("Lpg", mq5_value); 
+  ubidots.add("Air Quality", mq135_value);
+  ubidots.add("Rain", rain_value); 
+  ubidots.add("Temperature", t); 
+  ubidots.add("Humidity", h);
+  ubidots.add("Light", ldrvalue);
+  ubidots.add("Pressure", Pressure);
+  ubidots.add("Altitude", Altitude);
+  
+  bool bufferSent = false;
+  bufferSent = ubidots.send();
+  if (bufferSent) {
+   Serial.println("Values Was Sent To UBIDOTS !");
+  }
+ 
+ Serial.println();
+ Serial.print("1. Smoke ");Serial.print(mq2_value);Serial.println(" PPM");
+ Serial.print("2. cng ");Serial.print(mq4_value);Serial.println(" PPM");
+ Serial.print("3. lpg ");Serial.print(mq5_value);Serial.println("PPM");
+ Serial.print("4. airquality ");Serial.print(mq135_value);Serial.println(" PPM");
+ Serial.print("5. rainvalue ");Serial.print(rain_value);Serial.println(" MM");
+ Serial.print("6. temperature ");Serial.print(t);Serial.println(" C");
+ Serial.print("7. humidity ");Serial.print(h);Serial.println(" %");
+ Serial.print("8. ldr ");Serial.print(ldrvalue);Serial.println(" LX");
+ Serial.print("9. Pressure ");Serial.print(Pressure);Serial.println(" PA");
+ Serial.print("10. Altitude ");Serial.print(Altitude);Serial.println(" M");
+ Serial.println();
+
+ while (s_serial_to_nano.available() > 0)
+ s_serial_to_nano.read();
+
+   delay(3000);
+
+}
+}
+}
